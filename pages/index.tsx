d2f5e588c4636d5341f3878642bb844ecd809daf
TheHolisticTrader <122373954+Kapriel-Talatinian@users.noreply.github.com>
2025-07-01 10:23:43 +0200

    Add AskNostradamus UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import App from '../components/App';
+import AskNostradamus from '../components/AskNostradamus';
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
-      <App />
+      <AskNostradamus />
     </>
   );
 }
